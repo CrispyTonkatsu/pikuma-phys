ff8d47e49660dc70387e4d34121536a688ccec53
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2025-08-08 20:43:19 +0800

    Finished with most of the matrix functions Now we are just going to focus on keeping on with the course

--- src/Physics/matN.h
+++ src/Physics/matN.h
@@ -3,7 +3,7 @@
 
 #include <array>
 #include <cstddef>
-#include <ostream>
+#include <iostream>
 
 template<typename T, typename Y>
 using MULT_RETURN = decltype(std::declval<T>() * std::declval<Y>());
@@ -13,11 +13,11 @@ class matN;
 
 template<typename T, size_t W, size_t H>
 std::ostream& operator<<(std::ostream& os, matN<T, W, H> mat) {
-  for (size_t i = 0; i < W; i++) {
-    for (size_t j = 0; j < H - 1; j++) {
-      os << mat.at_mut(i, j) << " ";
+  for (size_t y = 0; y < H; y++) {
+    for (size_t x = 0; x < W - 1; x++) {
+      os << mat.at_mut(x, y) << " ";
     }
-    os << mat.at(i, H - 1) << std::endl;
+    os << mat.at(W - 1, y) << std::endl;
   }
 
   return os;
@@ -26,22 +26,98 @@ std::ostream& operator<<(std::ostream& os, matN<T, W, H> mat) {
 template<typename T, size_t W, size_t H>
 class matN {
   // TODO: Convert this from a 2d array to a 1d array for locality benefits (if
-  // there are any)
+  // there are any given std::array doesn't)
   std::array<std::array<T, H>, W> values{};
 
 public:
 
-  explicit matN(): values() {}
+  matN(): values() {}
+
+  [[nodiscard]] static auto Filled(const T& value) -> matN {
+    matN output{};
+
+    for (size_t x = 0; x < W; x++) {
+      for (size_t y = 0; y < H; y++) {
+        output.at_mut(x, y) = value;
+      }
+    }
+
+    return output;
+  }
+
+  [[nodiscard]] static auto Diagonal(const T& value) -> matN {
+    static_assert(W == H, "Only able to generate diagonal for square matrices");
+
+    matN output{};
+
+    for (size_t i = 0; i < W; i++) {
+      output.at_mut(i, i) = value;
+    }
+
+    return output;
+  }
 
   explicit matN(const std::array<std::array<T, H>, W>& values):
       values(values) {}
 
-  const T& at(size_t x, size_t y) const { return values[x][y]; }
+  matN(const matN& other) = default;
+  matN& operator=(const matN& other) = default;
+  matN(matN&&) = default;
+  matN& operator=(matN&&) = default;
+  ~matN() = default;
 
-  T& at_mut(size_t x, size_t y) { return values[y][x]; }
+  [[nodiscard]] auto at(size_t x, size_t y) const -> const T& {
+    return values[x][y];
+  }
+
+  [[nodiscard]] auto at_mut(size_t x, size_t y) -> T& { return values[x][y]; }
+
+  template<typename Y>
+  [[nodiscard]] auto operator+(const matN<Y, W, H>& other) const
+    -> matN<MULT_RETURN<T, Y>, W, H> {
+
+    using R = MULT_RETURN<T, Y>;
+    matN<R, W, H> output{};
+
+    for (size_t x = 0; x < W; x++) {
+      for (size_t y = 0; y < H; y++) {
+        output.at_mut(x, y) = at(x, y) + other.at(x, y);
+      }
+    }
+
+    return output;
+  }
+
+  [[nodiscard]] auto operator-() const -> matN {
+    matN<T, W, H> output{};
+
+    for (size_t x = 0; x < W; x++) {
+      for (size_t y = 0; y < H; y++) {
+        output.at_mut(x, y) = -at(x, y);
+      }
+    }
+
+    return output;
+  }
+
+  template<typename Y>
+  [[nodiscard]] auto operator-(const matN<Y, W, H>& other) const
+    -> matN<MULT_RETURN<T, Y>, W, H> {
+
+    using R = MULT_RETURN<T, Y>;
+    matN<R, W, H> output{};
+
+    for (size_t x = 0; x < W; x++) {
+      for (size_t y = 0; y < H; y++) {
+        output.at_mut(x, y) = at(x, y) - other.at(x, y);
+      }
+    }
+
+    return output;
+  }
 
   template<typename Y, size_t oW>
-  auto operator*(const matN<Y, oW, W>& other) const
+  [[nodiscard]] auto operator*(const matN<Y, oW, W>& other) const
     -> matN<MULT_RETURN<T, Y>, oW, H> {
 
     using R = MULT_RETURN<T, Y>;
@@ -57,8 +133,35 @@ public:
 
     return output;
   }
+
+  [[nodiscard]] auto operator*(float scale) const -> matN {
+    matN output{};
+
+    for (size_t x = 0; x < W; x++) {
+      for (size_t y = 0; y < H; y++) {
+        output.at_mut(x, y) = at(x, y) * scale;
+      }
+    }
+
+    return output;
+  }
+
+  [[nodiscard]] auto transpose() const -> matN<T, H, W> {
+    matN<T, H, W> output{};
+
+    for (size_t x = 0; x < W; x++) {
+      for (size_t y = 0; y < H; y++) {
+        output.at_mut(y, x) = at(x, y);
+      }
+    }
+
+    return output;
+  }
+
+  [[nodiscard]] auto is_square() const -> bool { return W == H; }
 };
 
+// TODO: Make this nice to use
 template<typename T, size_t N>
 using vecN = matN<T, 1, N>;
 
--- src/QuickTest.cpp
+++ src/QuickTest.cpp
@@ -3,21 +3,88 @@
 #include "Physics/matN.h"
 
 int main() {
-  std::cout << "Basic 2x2 test" << std::endl;
+  {
+    std::cout << "Basic 2x2 test" << std::endl;
 
-  matN<float, 2, 2> a{{{
-    {1.5f, 2.0f},
-    {0.5f, 1.f},
-  }}};
+    matN<float, 2, 2> a{{{
+      {1.5f, 2.0f},
+      {0.5f, 1.f},
+    }}};
 
-  matN<int, 2, 2> b{{{
-    {2, 0},
-    {0, 2},
-  }}};
+    matN<int, 2, 2> b{{{
+      {2, 0},
+      {0, 2},
+    }}};
 
-  std::cout << a * b << std::endl;
+    auto c = a * b;
 
-  // std::cout << "Basic 3x3 test" << std::endl;
+    // Checking the output matrix has the right dimensions
+    static_assert(!std::is_same_v<decltype(c), matN<int, 2, 2>>);
+    static_assert(std::is_same_v<decltype(c), matN<float, 2, 2>>);
+
+    // Checking calculation
+    std::cout << c << std::endl;
+  }
+
+  {
+    std::cout << "Basic 3x3 test" << std::endl;
+
+    matN<float, 3, 3> a{{{
+      {3, 3, 8},
+      {3, 4, 4},
+      {5, 2, 6},
+    }}};
+
+    matN<float, 3, 3> b{{{
+      {4, 4, 8},
+      {4, 8, 8},
+      {3, 8, 4},
+    }}};
+
+    auto c = a * b;
+
+    // Checking the output matrix has the right dimensions
+    static_assert(std::is_same_v<decltype(c), matN<float, 3, 3>>);
+
+    // Checking calculation
+    std::cout << c << std::endl;
+  }
+
+  {
+    std::cout << "2D Affine transformation test" << std::endl;
+
+    matN<float, 3, 3> a{{{
+      {2, 0, 0},
+      {0, 2, 0},
+      {3, -1, 1},
+    }}};
+
+    matN<float, 1, 3> b{{{
+      {2, 2, 1},
+    }}};
+
+    auto c = a * b;
+
+    // Checking the output matrix has the right dimensions
+    static_assert(std::is_same_v<decltype(c), matN<float, 1, 3>>);
+
+    // Checking calculation
+    std::cout << c << std::endl;
+  }
+
+  {
+    std::cout << "Negative, transpose, Mult test" << std::endl;
+
+    matN<float, 1, 3> a{{{{2, 2, 1}}}};
+
+    auto c = a * -a.transpose();
+
+    // Checking the output matrix has the right dimensions
+    static_assert(std::is_same_v<decltype(c), matN<float, 3, 3>>);
+
+    // Checking calculation
+    std::cout << c << std::endl;
+  }
 
   return 0;
 }
